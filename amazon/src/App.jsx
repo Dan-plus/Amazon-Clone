9442d377f1441b4fb917a6fc9c84f9a170434c5d
Dan-plus <danielgena88@gmail.com>
2025-09-18 01:45:26 -0500

    Carousel

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Componenets/Header/Header'
+import Carousel from "./Componenets/Carousel/Carousel";
+
 
 const App = () => {
   return (
     <div>
 <Header/>
+<Carousel/>
     </div>
   )
 }
