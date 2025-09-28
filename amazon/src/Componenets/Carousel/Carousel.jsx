1d0e94b12870653b8f87ebbe8bd3a95ec9875bec
Dan-plus <danielgena88@gmail.com>
2025-09-28 04:08:03 -0500

    carousel two

--- amazon/src/Componenets/Carousel/Carousel.jsx
+++ amazon/src/Componenets/Carousel/Carousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import { img } from "./BannerImg/data";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import classes from "./carousel.module.css";
+import classes from "./Carousel.module.css";
 
 const CarouselEffect = () => {
   return (
