30140adcf42ce218b574f49b340717732c233f05
Dan-plus <danielgena88@gmail.com>
2025-09-28 02:25:42 -0500

    axios

--- amazon/src/Api/axios.js
+++ amazon/src/Api/axios.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // 1 localhost
-  baseURL: "http://127.0.0.1:5001/clone-8628b/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-8628b/us-central1/api",
 
   // 2 deployed on firebase
   // baseURL: "",
 
   // 3 deployed on render.com
-//   baseURL: "",
+    baseURL: "https://amazon-clone-backend-mooc.onrender.com",
 });
 export { axiosInstance };
 
